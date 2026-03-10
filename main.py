48e1572c2f26c55d776f106a4cfee267e1ff5d5b
Willyang10x <willyangabrielfxt@gmail.com>
2026-03-09 23:10:10 -0300

    Subindo criacao de nova rota de chat

--- main.py
+++ main.py
@@ -1,7 +1,26 @@
+import os
 from fastapi import FastAPI
+from pydantic import BaseModel
+import google.generativeai as genai
+from dotenv import load_dotenv
+
+load_dotenv()
+
+genai.configure(api_key=os.environ["GEMINI_API_KEY"])
+
+model = genai.GenerativeModel('gemini-1.5-flash')
 
 app = FastAPI(title="Chatbot IA API")
+
+class MensagemUsuario(BaseModel):
+    texto: str
 
 @app.get("/")
 def read_root():
     return {"status": "ok", "mensagem": "Backend do Chatbot rodando com sucesso!"}
+
+@app.post("/chat")
+def conversar_com_ia(mensagem: MensagemUsuario):
+    resposta_ia = model.generate_content(mensagem.texto)
+    
+    return {"resposta": resposta_ia.text}
